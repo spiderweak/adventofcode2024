2f4b35eac8a227b2c6154cdf1ef330b5be821acc
Spiderweak <spiderweak@a-bernard.fr>
2024-12-03 15:12:40 -0500

    solve: day_01 part 1 (because it was easy)

--- days/day_03/src/main.rs
+++ days/day_03/src/main.rs
@@ -1,3 +1,68 @@
+use regex::Regex;
+
+use util::load_puzzle_example;
+use util::load_puzzle_input;
+
 fn main() {
-    println!("Hello, world!");
+    let day = "03";
+
+    match load_puzzle_example(day) {
+        Ok(example) => {
+            let (e_01, e_02) = solve(example);
+            println!("Example, part 1 : {}", e_01);
+            println!("Example, part 2 : {}", e_02);
+
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err)
+        }
+
+    }
+    match load_puzzle_input(day) {
+        Ok(input) => {
+            let (s_01, s_02) = solve(input);
+            println!("Solution, part 1 : {}", s_01);
+            println!("Solution, part 2 : {}", s_02);
+
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
+}
+
+fn solve(input: String) -> (i32, i32) {
+    let s_01: i32 = solve_01(&input);
+    let s_02: i32 = solve_02(&input);
+
+    return (s_01, s_02)
+}
+
+fn solve_01(input: &str) -> i32 {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    let mut results = vec![];
+    for (_, [val_01, val_02]) in re.captures_iter(input).map(|c| c.extract()) {
+        results.push((val_01.parse::<i32>().ok().unwrap(), val_02.parse::<i32>().ok().unwrap()));
+    }
+
+    results
+    .iter()
+    .map(|(a,b)| a*b)
+    .sum()
+}
+
+
+fn solve_02(input: &str) -> i32 {
+    let re = Regex::new(r"do\(\)*mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    let mut results = vec![];
+    for (_, [val_01, val_02]) in re.captures_iter(input).map(|c| c.extract()) {
+        results.push((val_01.parse::<i32>().ok().unwrap(), val_02.parse::<i32>().ok().unwrap()));
+    }
+
+    results
+    .iter()
+    .map(|(a,b)| a*b)
+    .sum()
 }
